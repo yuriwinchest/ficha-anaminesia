93b6c2b529a9ccc7622a75fbc0a8b2e404a53168
yuri <yuriallmeida@gmail.com>
2025-05-16 17:42:30 -0300

    Corrigir componente de data e restaurar chave API

---
+++ src/services/openAIService.ts
@@ -0,0 +1,4 @@
+import { HairAssessmentFormData } from "@/utils/formSchema"; 
+import { HairRecommendation } from "./hairRecommendationService"; 
+import { toast } from "@/components/ui/use-toast"; 
+ 
